06e76f3e1df185b326b1587baf3e80fab0726bec
Diogo Steinke Deconto <diogo.deconto@pf.up.edu.br>
2025-09-12 11:26:12 -0300

    API - Cadastrar, Listar e Buscar Produtos

--- Ecommerce/API/Models/Produto.cs
+++ Ecommerce/API/Models/Produto.cs
@@ -2,13 +2,14 @@ namespace API.Models;
 
 public class Produto
 {
+    //Construtor
     public Produto()
     {
         Id = Guid.NewGuid().ToString();
         CriadoEm = DateTime.Now;
     }
     public string Id { get; set; }
-    public string Nome { get; set; }
+    public string Nome { get; set; } = String.Empty;
     public int Quantidade { get; set; }
     public double Preco { get; set; }
     public DateTime CriadoEm { get; set; }
--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,10 +1,11 @@
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//Lista com produtos fakes
+//Lista com produtos fixos
 List<Produto> produtos = new List<Produto>
 {
     new Produto { Nome = "Notebook", Quantidade = 10, Preco = 3500.00 },
@@ -19,23 +20,51 @@ List<Produto> produtos = new List<Produto>
 // - Endereço/URL
 // - Método HTTP
 
+//Respostas
+// - Código de status HTTP
+// - Corpo/Dados
+
 //Métodos HTTP
-// GET: Recupera dados de um servidor sem modificar o recurso.
-// POST: Envia dados ao servidor para criar um novo recurso.
-// PUT: Atualiza completamente um recurso existente no servidor.
-// PATCH: Atualiza parcialmente um recurso existente.
-// DELETE: Remove um recurso do servidor.
+// GET: Buscar dados de um servidor sem modificar o recurso.
+// POST: Enviar dados ao servidor para criar um novo recurso.
+// PUT: Atualizar completamente um recurso existente no servidor.
+// PATCH: Atualizar parcialmente um recurso existente.
+// DELETE: Remover um recurso do servidor.
 app.MapGet("/", () => "API de Produtos");
 
 app.MapGet("/api/produto/listar", () =>
 {
-    return produtos;
+    //Validar se existe alguma coisa dentro da lista
+    if (produtos.Count > 0)
+    {
+        return Results.Ok(produtos);
+    }
+    return Results.BadRequest("Lista vazia");
 });
 
-app.MapPost("/api/produto/cadastrar", (Produto produto) =>
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
 {
+    foreach (Produto produtoCadastrado in produtos)
+    {
+        if (produtoCadastrado.Nome == produto.Nome)
+        {
+            return Results.Conflict("Produto já cadastrado!");
+        }
+    }
     produtos.Add(produto);
+    return Results.Created("", produto);
+});
+
+app.MapGet("/api/produto/buscar/{nome}", ([FromRoute] string nome) =>
+{
+    //Expressão lambda
+    // Produto produto = produtos.FirstOrDefault(x => x.Nome.Contains(nome));
+    Produto? resultado = produtos.FirstOrDefault(x => x.Nome == nome);
+    if (resultado == null)
+    {
+        return Results.NotFound("Produto não encontrado");
+    }
+    return Results.Ok(resultado);
 });
 
 app.Run();
-
